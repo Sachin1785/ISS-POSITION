a5c0e64de0f8a6bc84fe9cf42d224aac276adc92
Sachin1785 <sachhidanandamahapatro@gmail.com>
2024-06-27 14:28:02 +0530

    Added TIme in the output

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import requests
 import csv
 import time
+from datetime import datetime  # Import the datetime class
+
 def get_location(lat, lon, api_key):
     param = {
         "key": api_key,
@@ -9,21 +11,18 @@ def get_location(lat, lon, api_key):
     url = "https://api.opencagedata.com/geocode/v1/json"
     response = requests.get(url, params=param)
     data = response.json()
-    # print (data)
     if data['results']:
         location = data['results'][0]['formatted']
         return location
     else:
         return "Location not found."
 
-api_key = "991559085bf84fd3b0681008610eb690" 
+api_key = "991559085bf84fd3b0681008610eb690"
 
-
-while True :
+while True:
     resp = requests.get('http://api.open-notify.org/iss-now.json')
-    # resp.status_code
     print(resp.status_code)
-    print(f"Error:{resp.raise_for_status()}")
+    print(f"Error: {resp.raise_for_status()}")
     print(resp.json())
     lat = resp.json()['iss_position']['latitude']
     lon = resp.json()['iss_position']['longitude']
@@ -31,13 +30,17 @@ while True :
     print(lon)
 
     location = get_location(lat, lon, api_key)
-    print(location)
 
-    # Write data to CSV file
+    # Get the current date and time
+    now = datetime.now()
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    # Write data to CSV file including the current date and time
     filename = 'iss_location.csv'
-    with open(filename, mode='a', newline='',encoding='utf-8') as file:
+    with open(filename, mode='a', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        # writer.writerow(["Latitude", "Longitude", "Location"])
-        writer.writerow([lat, lon, location])
+        # Append the current date and time to the data being written
+        writer.writerow([lat, lon, location, current_time])
+
     print(f"Data written to '{filename}' successfully.")
-    time.sleep(30)
+    time.sleep(20)
